e5dce7d11678b2f86b6b9c8eb7e640d68282a426
Lava <svanbergsdottir@gmail.com>
2024-01-11 10:24:09 -1000

    first commit

---
+++ src/app/membershipdashboard.tsx
@@ -0,0 +1,19 @@
+<main className="details not-team logged-in">
+<div id="react-page"></div>
+
+  <input type="checkbox" className="modal-closing-trick" id="modal-closing-trick" />
+
+  <div className="overlay noscript-overlay" ></div>
+
+  <div className="modal modal-message group modal-warning">
+
+    <div className="modal-title">CodePen doesn't work very well without JavaScript.</div>
+
+    <p>We're all for progressive enhancement, but CodePen is a bit unique in that it's all about writing and showing front end code, including JavaScript. It's required to use most of the features of CodePen.</p>
+
+    <p>Need to know how to enable it? <a href="http://enable-javascript.com/" target="_blank" rel="noopener">Go here.</a></p>
+
+    <label className="button button-medium" htmlFor="modal-closing-trick">Close this, use anyway.</label>
+
+  </div>
+</main>
